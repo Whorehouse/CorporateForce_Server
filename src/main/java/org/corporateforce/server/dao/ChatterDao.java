744edbc947f88cdd78f7043c252d18d2c814e5c7
mrveress <veress.honaht@gmail.com>
2015-01-11 18:31:11 +0400

    Created ChatterDao

---
+++ src/main/java/org/corporateforce/server/dao/ChatterDao.java
@@ -0,0 +1,51 @@
+package org.corporateforce.server.dao;
+
+import java.util.List;
+
+import org.corporateforce.server.model.Chatter;
+import org.corporateforce.server.model.Users;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
+
+public class ChatterDao extends AbstractDao<Chatter> {
+	public ChatterDao() {
+		super(Chatter.class);
+	}
+
+	public ChatterDao(Class<Chatter> entityClass) {
+		super(entityClass);
+	}
+	
+	public List<Chatter> getListForParent(Users u) {
+		List<Chatter> entityList = null;
+		Session session = sessionFactory.openSession();
+		Transaction tx = session.beginTransaction();
+		try { 
+			entityList = session.createCriteria(entityClass)
+				.add(Restrictions.eq("chatter.usersByParent", u)).list();
+			tx.commit();			
+		} catch (Exception e) {
+			tx.rollback();
+		} finally {
+			session.close();
+		}
+		return entityList;
+	}
+	
+	public List<Chatter> getListForCreator(Users u) {
+		List<Chatter> entityList = null;
+		Session session = sessionFactory.openSession();
+		Transaction tx = session.beginTransaction();
+		try { 
+			entityList = session.createCriteria(entityClass)
+				.add(Restrictions.eq("chatter.usersByCreator", u)).list();
+			tx.commit();			
+		} catch (Exception e) {
+			tx.rollback();
+		} finally {
+			session.close();
+		}
+		return entityList;
+	}
+}
