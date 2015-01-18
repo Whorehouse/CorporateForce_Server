2af21c939dc1a4b366284e6cb8ce664606cb2723
ChepKun <ProgrammerKun@gmail.com>
2015-01-19 01:06:39 +0300

    Updated beans.

--- src/main/java/org/corporateforce/server/jsf/ProfilesBean.java
+++ src/main/java/org/corporateforce/server/jsf/ProfilesBean.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.faces.context.FacesContext;
+
 import org.corporateforce.server.dao.ProfilesDao;
 import org.corporateforce.server.model.Profiles;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,12 +21,35 @@ public class ProfilesBean implements Serializable {
 	@Autowired
 	private ProfilesDao profilesDao;
 
+	private Profiles editProfile;
+
+	public Profiles getEditProfile() {
+		return editProfile;
+	}
+
+	public void setEditProfile(Profiles editProfile) {
+		this.editProfile = editProfile;
+	}
+
+	private List<Profiles> profilesList = null;
+	
 	public ProfilesDao getProfilesDao() {
 		return profilesDao;
 	}
 
 	public void setProfilesDao(ProfilesDao profilesDao) {
 		this.profilesDao = profilesDao;
+	}
+	
+	public List<Profiles> getProfilesList() throws Exception {
+		if (profilesList != null)
+			return profilesList;
+		profilesList = profilesDao.getEntityList();
+		return profilesList;
+	}
+
+	public void refreshProfilesList() throws Exception {
+		profilesList = profilesDao.getEntityList();
 	}
 	
 	public Map<String, Integer> getProfilesMap() throws Exception {
@@ -34,5 +59,37 @@ public class ProfilesBean implements Serializable {
 			result.put(p.getName(), p.getId());
 		}
 		return result;
+	}
+	
+	public void actionEdit() {
+		FacesContext fc = FacesContext.getCurrentInstance();
+		Map<String, String> params = fc.getExternalContext().getRequestParameterMap();
+		String id = params.get("editProfileId");
+		try {
+			this.setEditProfile(profilesDao.getEntityById(Integer.parseInt(id)));
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void saveEditProfile() throws Exception {
+		profilesDao.updateEntity(editProfile);
+		refreshProfilesList();
+	}
+	
+	public void actionDelete() {
+		FacesContext fc = FacesContext.getCurrentInstance();
+		Map<String, String> params = fc.getExternalContext().getRequestParameterMap();
+		String id = params.get("deleteProfileId");
+		try {
+			profilesDao.deleteEntity(Integer.parseInt(id));
+			refreshProfilesList();
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
 	}
 }
--- src/main/java/org/corporateforce/server/jsf/RolesBean.java
+++ src/main/java/org/corporateforce/server/jsf/RolesBean.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.faces.context.FacesContext;
+
 import org.corporateforce.server.dao.RolesDao;
 import org.corporateforce.server.model.Roles;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +21,10 @@ public class RolesBean implements Serializable {
 	@Autowired
 	private RolesDao rolesDao;
 
+	private Roles editRole;
+	
+	private List<Roles> rolesList = null;
+	
 	public RolesDao getRolesDao() {
 		return rolesDao;
 	}
@@ -36,4 +42,60 @@ public class RolesBean implements Serializable {
 		return result;
 	}
 
+	/**
+	 * @return the editRole
+	 */
+	public Roles getEditRole() {
+		return editRole;
+	}
+
+	/**
+	 * @param editRole the editRole to set
+	 */
+	public void setEditRole(Roles editRole) {
+		this.editRole = editRole;
+	}
+
+	public List<Roles> getRolesList() throws Exception {
+		if (rolesList != null)
+			return rolesList;
+		rolesList = rolesDao.getEntityList();
+		return rolesList;
+	}
+
+	public void refreshRolesList() throws Exception {
+		rolesList = rolesDao.getEntityList();
+	}
+	
+	public void actionEdit() {
+		FacesContext fc = FacesContext.getCurrentInstance();
+		Map<String, String> params = fc.getExternalContext().getRequestParameterMap();
+		String id = params.get("editRoleId");
+		try {
+			this.setEditRole(rolesDao.getEntityById(Integer.parseInt(id)));
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void saveEditRole() throws Exception {
+		rolesDao.updateEntity(editRole);
+		refreshRolesList();
+	}
+	
+	public void actionDelete() {
+		FacesContext fc = FacesContext.getCurrentInstance();
+		Map<String, String> params = fc.getExternalContext().getRequestParameterMap();
+		String id = params.get("deleteRoleId");
+		try {
+			rolesDao.deleteEntity(Integer.parseInt(id));
+			refreshRolesList();
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+	}
 }
