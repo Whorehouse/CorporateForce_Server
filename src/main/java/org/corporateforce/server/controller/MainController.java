9213b5760f9966f2390be8a903a3911b4df93d5f
ChepKun <ProgrammerKun@gmail.com>
2015-05-16 14:26:04 -0300

    Sidebar is working now.

--- src/main/java/org/corporateforce/server/controller/MainController.java
+++ src/main/java/org/corporateforce/server/controller/MainController.java
@@ -17,6 +17,10 @@ public class MainController extends AbstractController implements Serializable {
 	private final Integer SIDEBAR_LINK_NOTSELECTED = 0;
 	private final Integer SIDEBAR_LINK_USERS = 1;
 	private final Integer SIDEBAR_LINK_PROFILES = 2;
+	private final Integer SIDEBAR_LINK_ROLES = 3;
+	private final Integer SIDEBAR_LINK_OFFICES = 4;
+	private final Integer SIDEBAR_LINK_SETTINGS = 5;
+	private final Integer SIDEBAR_LINK_SUPPORT = 6;
 
 	// page variables
 	
@@ -27,6 +31,8 @@ public class MainController extends AbstractController implements Serializable {
 	}
 	
 	// controller methods
+
+	// actived links
 
 	public Boolean sidebarLinkNotSelected() {
 		return selectedSidebarLink == SIDEBAR_LINK_NOTSELECTED;
@@ -40,7 +46,46 @@ public class MainController extends AbstractController implements Serializable {
 		return selectedSidebarLink == SIDEBAR_LINK_PROFILES;
 	}
 
+	public Boolean sidebarLinkRoles() {
+		return selectedSidebarLink == SIDEBAR_LINK_ROLES;
+	}
+
+	public Boolean sidebarLinkOffices() {
+		return selectedSidebarLink == SIDEBAR_LINK_OFFICES;
+	}
+
+	public Boolean sidebarLinkSettings() {
+		return selectedSidebarLink == SIDEBAR_LINK_SETTINGS;
+	}
+
+	public Boolean sidebarLinkSupport() {
+		return selectedSidebarLink == SIDEBAR_LINK_SUPPORT;
+	}
+
+	// select links
+
 	public void sidebarSelectLinkUsers() {
 		this.selectedSidebarLink = SIDEBAR_LINK_USERS;
 	}
+
+	public void sidebarSelectLinkProfiles() {
+		this.selectedSidebarLink = SIDEBAR_LINK_PROFILES;
+	}
+
+	public void sidebarSelectLinkRoles() {
+		this.selectedSidebarLink = SIDEBAR_LINK_ROLES;
+	}
+
+	public void sidebarSelectLinkOffices() {
+		this.selectedSidebarLink = SIDEBAR_LINK_OFFICES;
+	}
+
+	public void sidebarSelectLinkSettings() {
+		this.selectedSidebarLink = SIDEBAR_LINK_SETTINGS;
+	}
+
+	public void sidebarSelectLinkSupport() {
+		this.selectedSidebarLink = SIDEBAR_LINK_SUPPORT;
+	}
+
 }
